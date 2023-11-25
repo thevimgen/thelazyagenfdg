648c86263184d577be7e2206cc19b8c1b9d9468a
a3 <a3@a3.a3>
2023-11-25 22:52:40 +0200

    init workflow 3

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { peerDependencies } from "./package.json";
 export default defineConfig({
   build: {
     lib: {
-      entry: "./src/index.ts", // Specifies the entry point for building the library.
+      entry: "./libs/index.ts", // Specifies the entry point for building the library.
       name: "vite-react-ts-button", // Sets the name of the generated library.
       fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
       formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
