a5592c8286f36aff0d3295b0c1f3ea79e7e5aaf4
a3 <a3@a3.a3>
2023-11-26 11:08:19 +0200

    fix npm errors 1

--- setupTests.ts
+++ setupTests.ts
@@ -2,7 +2,7 @@ import { expect } from "vitest";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 declare module "vitest" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-anyd
   interface Assertion<T = any>
     extends jest.Matchers<void, T>,
       TestingLibraryMatchers<T, void> {}
